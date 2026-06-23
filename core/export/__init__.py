203222d9b6c8a7f25f4b36158d452215d69eb90c
yan-sln <yan.sln@outlook.com>
2026-06-23 11:47:19 +0200

    1. Remove GeoPDF and change panel. fix #24

--- core/export/__init__.py
+++ core/export/__init__.py
@@ -4,11 +4,11 @@ Provides convenient imports for CSV, GeoPDF and multi-page PDF export functions.
 """
 
 from .csv.export import export_results_to_csv
-from .pdf.geopdf import export_results_to_pdf
+from .pdf.legend import export_legend
 from .pdf.multi_pdf import export_results_to_multi_page_pdf
 
 __all__ = [
     "export_results_to_csv",
-    "export_results_to_pdf",
+    "export_legend",
     "export_results_to_multi_page_pdf",
 ]
